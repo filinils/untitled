616edddbf619d29207d87424313d99e141916dca
Pedersen <jerry.pedersen@capgemini.com>
2017-10-09 10:49:36 +0200

    Add three-react-mtl-loader and three-obj-loader. Add init.js and loadModel function

--- client/src/components/pages/ThreeViewer/ThreeViewer.js
+++ client/src/components/pages/ThreeViewer/ThreeViewer.js
@@ -3,233 +3,250 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { loadModel } from "./init";
 
 class ThreeViewer extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.camera, this.scene, this.renderer, this.helper;
-    this.plane, this.cube;
-    this.mouse, this.raycaster, (this.isShiftDown = false);
-    this.rollOverMesh, this.rollOverMaterial;
-    this.cubeGeo, this.cubeMaterial;
-    this.objects = [];
+		this.camera, this.scene, this.renderer, this.helper;
+		this.plane, this.cube;
+		this.mouse, this.raycaster, (this.isShiftDown = false);
+		this.rollOverMesh, this.rollOverMaterial;
+		this.cubeGeo, this.cubeMaterial;
+		this.objects = [];
 
-    this.info = {};
+		this.info = {};
 
-    this.init = this.init.bind(this);
+		this.init = this.init.bind(this);
 
-    this.onDocumentMouseMove = this.onDocumentMouseMove.bind(this);
-    this.onDocumentMouseDown = this.onDocumentMouseDown.bind(this);
-    this.onDocumentKeyDown = this.onDocumentKeyDown.bind(this);
-    this.onDocumentKeyUp = this.onDocumentKeyUp.bind(this);
+		this.onDocumentMouseMove = this.onDocumentMouseMove.bind(this);
+		this.onDocumentMouseDown = this.onDocumentMouseDown.bind(this);
+		this.onDocumentKeyDown = this.onDocumentKeyDown.bind(this);
+		this.onDocumentKeyUp = this.onDocumentKeyUp.bind(this);
 
-    this.onWindowResize = this.onWindowResize.bind(this);
-  }
+		this.onWindowResize = this.onWindowResize.bind(this);
+	}
 
-  componentDidMount() {
-    this.init();
-    this.render3D();
-  }
+	componentDidMount() {
+		this.init();
+		this.render3D();
+	}
 
-  render3D() {
-    if (this.renderer) {
-      this.renderer.render(this.scene, this.camera);
-    }
-  }
+	render3D() {
+		if (this.renderer) {
+			this.renderer.render(this.scene, this.camera);
+		}
+	}
 
-  infoElement(style, innerHtml) {
-    return <div style={style} dangerouslySetInnerHTML={innerHtml} />;
-  }
+	infoElement(style, innerHtml) {
+		return <div style={style} dangerouslySetInnerHTML={innerHtml} />;
+	}
 
-  init() {
-    let infoStyle = {
-      position: "absolute",
-      top: "10px",
-      width: "100%",
-      textAlign: "center"
-    };
+	init() {
+		let infoStyle = {
+			position: "absolute",
+			top: "10px",
+			width: "100%",
+			textAlign: "center"
+		};
 
-    let infoInnerHtml =
-      '<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> - voxel painter - webgl<br><strong>click</strong>: add voxel, <strong>shift + click</strong>: remove voxel';
+		let infoInnerHtml =
+			'<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> - voxel painter - webgl<br><strong>click</strong>: add voxel, <strong>shift + click</strong>: remove voxel';
 
-    this.info = this.infoElement(infoStyle, infoInnerHtml);
+		this.info = this.infoElement(infoStyle, infoInnerHtml);
 
-    this.camera = new THREE.PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      1,
-      10000
-    );
+		this.camera = new THREE.PerspectiveCamera(
+			45,
+			window.innerWidth / window.innerHeight,
+			1,
+			10000
+		);
 
-    this.camera.position.set(300, 500, 1300);
-    this.camera.lookAt(new THREE.Vector3());
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xf0f0f0);
+		this.camera.position.set(300, 500, 1300);
+		this.camera.lookAt(new THREE.Vector3());
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0xf0f0f0);
 
-    // roll-over helpers
-    let rollOverGeo = new THREE.BoxGeometry(50, 50, 50);
+		// roll-over helpers
+		let rollOverGeo = new THREE.BoxGeometry(50, 50, 50);
 
-    this.rollOverMaterial = new THREE.MeshBasicMaterial({
-      color: 0xff0000,
-      opacity: 0.5,
-      transparent: true
-    });
+		this.rollOverMaterial = new THREE.MeshBasicMaterial({
+			color: 0xff0000,
+			opacity: 0.5,
+			transparent: true
+		});
 
-    this.helper = new THREE.CameraHelper(this.camera);
-    this.scene.add(this.helper);
-    this.rollOverMesh = new THREE.Mesh(rollOverGeo, this.rollOverMaterial);
-    this.scene.add(this.rollOverMesh);
+		this.helper = new THREE.CameraHelper(this.camera);
+		this.scene.add(this.helper);
+		// this.rollOverMesh = new THREE.Mesh(rollOverGeo, this.rollOverMaterial);
+		// this.rollOverMesh =
+		loadModel("male02", object => {
+			this.rollOverMesh = object;
+			console.log("LOADED", object);
 
-    // cubes
-    this.cubeGeo = new THREE.BoxGeometry(50, 50, 50);
-    this.cubeMaterial = new THREE.MeshLambertMaterial({
-      color: 0xfeb74c,
-      map: new THREE.TextureLoader().load("textures/square-outline-texture.png")
-    });
+			this.scene.add(this.rollOverMesh);
+		});
 
-    // grid
-    let gridHelper = new THREE.GridHelper(1000, 20);
-    this.scene.add(gridHelper);
+		// cubes
+		this.cubeGeo = new THREE.BoxGeometry(50, 50, 50);
+		this.cubeMaterial = new THREE.MeshLambertMaterial({
+			color: 0xfeb74c,
+			map: new THREE.TextureLoader().load(
+				"textures/square-outline-texture.png"
+			)
+		});
 
-    //
-    this.raycaster = new THREE.Raycaster();
-    this.mouse = new THREE.Vector2();
+		// grid
+		let gridHelper = new THREE.GridHelper(1000, 20);
+		this.scene.add(gridHelper);
 
-    let geometry = new THREE.PlaneBufferGeometry(1000, 1000);
-    geometry.rotateX(-Math.PI / 2);
+		//
+		this.raycaster = new THREE.Raycaster();
+		this.mouse = new THREE.Vector2();
 
-    this.plane = new THREE.Mesh(
-      geometry,
-      new THREE.MeshBasicMaterial({
-        visible: false
-      })
-    );
+		let geometry = new THREE.PlaneBufferGeometry(1000, 1000);
+		geometry.rotateX(-Math.PI / 2);
 
-    this.scene.add(this.plane);
-    this.objects.push(this.plane);
+		this.plane = new THREE.Mesh(
+			geometry,
+			new THREE.MeshBasicMaterial({
+				visible: false
+			})
+		);
 
-    // Lights
-    let ambientLight = new THREE.AmbientLight(0x606060);
-    this.scene.add(ambientLight);
+		this.scene.add(this.plane);
+		this.objects.push(this.plane);
 
-    let directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(1, 0.75, 0.5).normalize();
+		// Lights
+		let ambientLight = new THREE.AmbientLight(0x606060);
+		this.scene.add(ambientLight);
 
-    this.scene.add(directionalLight);
-    this.renderer = new THREE.WebGLRenderer({
-      antialias: true
-    });
+		let directionalLight = new THREE.DirectionalLight(0xffffff);
+		directionalLight.position.set(1, 0.75, 0.5).normalize();
 
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.container.appendChild(this.renderer.domElement);
+		this.scene.add(directionalLight);
+		this.renderer = new THREE.WebGLRenderer({
+			antialias: true
+		});
 
-    document.addEventListener("mousemove", this.onDocumentMouseMove, false);
-    document.addEventListener("mousedown", this.onDocumentMouseDown, false);
-    document.addEventListener("keydown", this.onDocumentKeyDown, false);
-    document.addEventListener("keyup", this.onDocumentKeyUp, false);
-    //
-    window.addEventListener("resize", this.onWindowResize, false);
-  }
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.container.appendChild(this.renderer.domElement);
 
-  onDocumentMouseMove() {
-    event.preventDefault();
-    this.mouse.set(
-      event.clientX / window.innerWidth * 2 - 1,
-      -(event.clientY / window.innerHeight) * 2 + 1
-    );
+		document.addEventListener("mousemove", this.onDocumentMouseMove, false);
+		document.addEventListener("mousedown", this.onDocumentMouseDown, false);
+		document.addEventListener("keydown", this.onDocumentKeyDown, false);
+		document.addEventListener("keyup", this.onDocumentKeyUp, false);
+		//
+		window.addEventListener("resize", this.onWindowResize, false);
+	}
 
-    this.raycaster.setFromCamera(this.mouse, this.camera);
-    let intersects = this.raycaster.intersectObjects(this.objects);
+	onDocumentMouseMove() {
+		event.preventDefault();
+		this.mouse.set(
+			event.clientX / window.innerWidth * 2 - 1,
+			-(event.clientY / window.innerHeight) * 2 + 1
+		);
 
-    if (intersects.length > 0) {
-      let intersect = intersects[0];
-      this.rollOverMesh.position
-        .copy(intersect.point)
-        .add(intersect.face.normal);
-      this.rollOverMesh.position
-        .divideScalar(50)
-        .floor()
-        .multiplyScalar(50)
-        .addScalar(25);
-    }
+		this.raycaster.setFromCamera(this.mouse, this.camera);
+		let intersects = this.raycaster.intersectObjects(this.objects);
 
-    this.container.appendChild(this.renderer.domElement);
-    this.render3D();
-  }
+		if (intersects.length > 0) {
+			let intersect = intersects[0];
+			this.rollOverMesh.position
+				.copy(intersect.point)
+				.add(intersect.face.normal);
+			this.rollOverMesh.position
+				.divideScalar(50)
+				.floor()
+				.multiplyScalar(50)
+				.addScalar(25);
+		}
 
-  onDocumentMouseDown() {
-    event.preventDefault();
-    this.mouse.set(
-      event.clientX / window.innerWidth * 2 - 1,
-      -(event.clientY / window.innerHeight) * 2 + 1
-    );
+		this.container.appendChild(this.renderer.domElement);
+		this.render3D();
+	}
 
-    this.raycaster.setFromCamera(this.mouse, this.camera);
-    let intersects = this.raycaster.intersectObjects(this.objects);
-    if (intersects.length > 0) {
-      let intersect = intersects[0];
-      // delete cube
-      if (this.isShiftDown) {
-        if (intersect.object != this.plane) {
-          this.scene.remove(intersect.object);
-          this.objects.splice(this.objects.indexOf(intersect.object), 1);
-        }
-        // create cube
-      } else {
-        let voxel = new THREE.Mesh(this.cubeGeo, this.cubeMaterial);
-        voxel.position.copy(intersect.point).add(intersect.face.normal);
-        voxel.position
-          .divideScalar(50)
-          .floor()
-          .multiplyScalar(50)
-          .addScalar(25);
-        this.scene.add(voxel);
-        this.objects.push(voxel);
-      }
-      this.render3D();
-    }
-  }
+	onDocumentMouseDown() {
+		event.preventDefault();
+		this.mouse.set(
+			event.clientX / window.innerWidth * 2 - 1,
+			-(event.clientY / window.innerHeight) * 2 + 1
+		);
 
-  onDocumentKeyDown() {
-    switch (event.keyCode) {
-      case 16:
-        this.isShiftDown = true;
-        break;
-    }
-  }
+		this.raycaster.setFromCamera(this.mouse, this.camera);
+		let intersects = this.raycaster.intersectObjects(this.objects);
+		if (intersects.length > 0) {
+			let intersect = intersects[0];
+			// delete cube
+			if (this.isShiftDown) {
+				if (intersect.object != this.plane) {
+					this.scene.remove(intersect.object);
+					this.objects.splice(
+						this.objects.indexOf(intersect.object),
+						1
+					);
+				}
+				// create cube
+			} else {
+				// let voxel = new THREE.Mesh(this.cubeGeo, this.cubeMaterial);
+				loadModel("male02", voxel => {
+					console.log(voxel);
+					voxel.position
+						.copy(intersect.point)
+						.add(intersect.face.normal);
+					voxel.position
+						.divideScalar(50)
+						.floor()
+						.multiplyScalar(50)
+						.addScalar(25);
+					this.scene.add(voxel);
+					this.objects.push(voxel);
+				});
+			}
+			this.render3D();
+		}
+	}
 
-  onDocumentKeyUp() {
-    switch (event.keyCode) {
-      case 16:
-        this.isShiftDown = false;
-        break;
-    }
-  }
+	onDocumentKeyDown() {
+		switch (event.keyCode) {
+			case 16:
+				this.isShiftDown = true;
+				break;
+		}
+	}
 
-  onWindowResize() {
-    this.camera.aspect = window.innerWidth / window.innerHeight;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-  }
+	onDocumentKeyUp() {
+		switch (event.keyCode) {
+			case 16:
+				this.isShiftDown = false;
+				break;
+		}
+	}
 
-  render() {
-    return (
-      <div
-        className="_layout-diagram"
-        ref={el => {
-          this.container = el;
-        }}
-      />
-    );
-  }
+	onWindowResize() {
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+
+	render() {
+		return (
+			<div
+				className="_layout-diagram"
+				ref={el => {
+					this.container = el;
+				}}
+			/>
+		);
+	}
 }
 
 ThreeViewer.propTypes = {};
 
 function mapStateToProps(state, ownProps) {
-  return {};
+	return {};
 }
 
 export default connect(mapStateToProps)(ThreeViewer);
--- client/src/components/pages/ThreeViewer/init.js
+++ client/src/components/pages/ThreeViewer/init.js
@@ -1,8 +1,8 @@
-import * as THREE from "three";
+import * as Three from "three";
 import MTLLoader from "three-react-mtl-loader";
 // import OBJLoader from "three-react-obj-loader";
 import OBJLoader from "three-obj-loader";
-OBJLoader(THREE);
+OBJLoader(Three);
 
 function init(data) {
 	var container, stats;
@@ -11,26 +11,26 @@ function init(data) {
 	container = document.createElement("div");
 	document.body.appendChild(container);
 
-	camera = new THREE.PerspectiveCamera(
+	camera = new Three.PerspectiveCamera(
 		45,
 		window.innerWidth / window.innerHeight,
 		1,
 		10000
 	);
 	camera.position.set(500, 800, 1300);
-	camera.lookAt(new THREE.Vector3());
-	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0xf0f0f0);
+	camera.lookAt(new Three.Vector3());
+	scene = new Three.Scene();
+	scene.background = new Three.Color(0xf0f0f0);
 
-	// var ambient = new THREE.AmbientLight(0x444444);
+	// var ambient = new Three.AmbientLight(0x444444);
 	// scene.add(ambient);
-	// var directionalLight = new THREE.DirectionalLight(0xffeedd);
+	// var directionalLight = new Three.DirectionalLight(0xffeedd);
 	// directionalLight.position.set(0, 0, 1).normalize();
 	// scene.add(directionalLight);
 	// model
 
 	//
-	renderer = new THREE.WebGLRenderer();
+	renderer = new Three.WebGLRenderer();
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	container.appendChild(renderer.domElement);
@@ -38,7 +38,7 @@ function init(data) {
 	return { container, stats, camera, scene, renderer };
 }
 
-export const loadModel = (point, modelName, scene) => {
+export const loadModel = (modelName, callback) => {
 	var onProgress = function(xhr) {
 		if (xhr.lengthComputable) {
 			var percentComplete = xhr.loaded / xhr.total * 100;
@@ -52,21 +52,41 @@ export const loadModel = (point, modelName, scene) => {
 	mtlLoader.setPath("assets/");
 	mtlLoader.load(modelName + ".mtl", function(materials) {
 		materials.preload();
-		let objLoader = new THREE.OBJLoader();
+		let objLoader = new Three.OBJLoader();
 		objLoader.setMaterials(materials);
 		objLoader.setPath("assets/");
 		objLoader.load(
 			modelName + ".obj",
 			function(object) {
-				object.position.y = point.y;
-				object.position.x = point.x;
-				object.position.z = point.z;
-				scene.add(object);
+				callback(object);
 			},
 			onProgress,
 			onError
 		);
 	});
+};
+
+export const loadWireframeModel = (modelName, callback) => {
+	var onProgress = function(xhr) {
+		if (xhr.lengthComputable) {
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log(Math.round(percentComplete, 2) + "% downloaded");
+		}
+	};
+
+	var onError = function(xhr) {};
+	let material = new Three();
+	let objLoader = new Three.OBJLoader();
+	objLoader.setMaterials(materials);
+	objLoader.setPath("assets/");
+	objLoader.load(
+		modelName + ".obj",
+		function(object) {
+			callback(object);
+		},
+		onProgress,
+		onError
+	);
 };
 
 export default init;
