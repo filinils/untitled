4642d2e8a7dbec9b674617b44e371f340d86ee01
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2017-10-20 15:39:49 +0200

    Change unit to cm

--- client/src/blueprint/core/configuration.js
+++ client/src/blueprint/core/configuration.js
@@ -15,45 +15,45 @@ export const configWallThickness = "wallThickness";
 
 /** Global configuration to customize the whole system.  */
 export default (() => {
-  /** Configuration data loaded from/stored to extern. */
-  const data = {
-    dimUnit: Dimensioning.dimInch,
+	/** Configuration data loaded from/stored to extern. */
+	const data = {
+		dimUnit: Dimensioning.dimCentiMeter,
 
-    wallHeight: 250,
-    wallThickness: 10
-  };
+		wallHeight: 250,
+		wallThickness: 10
+	};
 
-  /** Set a configuration parameter. */
-  function setValue(key, value) {
-    this.data[key] = value;
-  }
+	/** Set a configuration parameter. */
+	function setValue(key, value) {
+		this.data[key] = value;
+	}
 
-  /** Get a string configuration parameter. */
-  function getStringValue(key) {
-    switch (key) {
-      case configDimUnit:
-        return data[key];
-      default:
-        throw new Error("Invalid string configuration parameter: " + key);
-    }
-  }
+	/** Get a string configuration parameter. */
+	function getStringValue(key) {
+		switch (key) {
+			case configDimUnit:
+				return data[key];
+			default:
+				throw new Error(
+					"Invalid string configuration parameter: " + key
+				);
+		}
+	}
 
-  /** Get a numeric configuration parameter. */
-  function getNumericValue(key) {
-    switch (key) {
-      case configWallHeight:
-      case configWallThickness:
-        return data[key];
-      default:
-        throw new Error("Invalid numeric configuration parameter: " + key);
-    }
-  }
-  return {
-
-    getNumericValue,
-    getStringValue,
-    
-
-
-  }
+	/** Get a numeric configuration parameter. */
+	function getNumericValue(key) {
+		switch (key) {
+			case configWallHeight:
+			case configWallThickness:
+				return data[key];
+			default:
+				throw new Error(
+					"Invalid numeric configuration parameter: " + key
+				);
+		}
+	}
+	return {
+		getNumericValue,
+		getStringValue
+	};
 })();
