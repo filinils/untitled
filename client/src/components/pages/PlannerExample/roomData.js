5c8af91c59716f7b486a2d3c82692dede92141fc
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2017-10-24 12:35:45 +0200

    Remove window

--- client/src/components/pages/PlannerExample/roomData.js
+++ client/src/components/pages/PlannerExample/roomData.js
@@ -69,38 +69,6 @@ export default `{
 			"url":"assets/models/IKEA/bed_roughness_1024.jpg"
 		}
 	]},
-		 
-		{
-			"item_name": "Window",
-			"item_type": 3,
-			"model_url": "assets/models/Window/Window_geo/Window_geo.fbx",
-			"xpos": 886.8841174461031,
-			"ypos": 139.1510114697785,
-			"zpos": -105.16400146484375,
-			"rotation": 0,
-			"scale_x": 1,
-			"scale_y": 1,
-			"scale_z": 1,
-			"fixed": false,
-			"texture_maps":[
-				{
-				"type":"map",
-				"url":"assets/models/Window/Window_textures/1024/Window_geo_window_frame_mat_BaseColor.jpg"
-			},
-				{
-				"type":"normal",
-				"url":"assets/models/Window/Window_textures/1024/Window_geo_window_frame_mat_Normal.jpg"
-			},
-				{
-				"type":"metallic",
-				"url":"assets/models/Window/Window_textures/1024/Window_geo_window_frame_mat_Metallic.jpg"
-			},
-				{
-				"type":"roughness",
-				"url":"assets/models/Window/Window_textures/1024/Window_geo_window_frame_mat_Roughness.jpg"
-			}
-		]
-			},
 	 
 			{
 					"item_name": "Floor Lamp",
